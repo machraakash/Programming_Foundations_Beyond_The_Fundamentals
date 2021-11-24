10672bf0549f6ea59c79c32b6f3ffe51b32ba09c
machraakash <34833061+machraakash@users.noreply.github.com>
2021-11-24 16:20:43 +0530

    Add files via upload

---
+++ List_Class.py
@@ -0,0 +1,13 @@
+players = [
+    'Messi',
+    'Ramos',
+    'Cristiano',
+    'Mbappe',
+    'De Bruyne',
+    'Neymar',
+    'Modric',
+    'Kante',
+]
+
+print(players.count('Messi'))
+print(players.pop())
